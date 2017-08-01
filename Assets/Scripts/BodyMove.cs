4a16c1cdecbe164884b2a8609ef33840a6380217
Ldrumstick <liaochengtao@foxmail.com>
2017-08-01 13:07:58 +0800

    更新了场景

---
+++ Assets/Scripts/BodyMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyMove : MonoBehaviour {
+
+    public float moveSpeed = 5;
+    private HeadMove head;
+    private Transform body;
+    private snake m_snake;
+
+    void Start()
+    {
+        head = GetComponent<HeadMove>();
+        body = this.GetComponent<Transform>();
+        m_snake.body = GetComponent<GameObject>();
+    }
+
+    void Update()
+    {
+        /*
+        if(Vector3.Distance(head.oldPosition,body.position) >= 2)
+            body.Translate(Vector3.Normalize(transform.position - head.oldPosition) * Time.deltaTime * moveSpeed);*/
+        if (head.isCreate == true && m_snake.nextbody == null)
+        {
+            m_snake.nextbody = head.snakebody;
+        }
+        if (m_snake.nextbody != null)
+        {
+            if (Vector3.Distance(m_snake.body.transform.position, m_snake.nextbody.transform.position) >= 2)
+            {
+                m_snake.nextbody.transform.Translate(Vector3.Normalize(m_snake.nextbody.transform.position - m_snake.body.transform.position) * Time.deltaTime * moveSpeed);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/HeadMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct snake
+{
+    public GameObject body;
+    public GameObject nextbody;
+}
+public class HeadMove : MonoBehaviour {
+
+    public float moveSpeed = 5;
+    public Vector3 oldPosition;
+    public GameObject snakebody;
+    public bool isCreate = false;
+
+    void Start()
+    {
+        oldPosition = transform.position;
+    }
+
+    void Update()
+    {
+
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(h, 0, v) * Time.deltaTime * moveSpeed);
+        if (Vector3.Distance(oldPosition, transform.position) >= 2)
+        {
+            oldPosition = transform.position;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            //Instantiate(body, head.oldPosition, Quaternion.identity);
+            Instantiate(snakebody, oldPosition, Quaternion.identity);
+            isCreate = true;
+        }
+        isCreate = false;   
+    }
+}
